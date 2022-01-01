f9eb9648a007fc4deb64d943e8da2b1081439d9b
Divyesh Patel <71879941+DD-PATEL@users.noreply.github.com>
2022-01-01 10:46:14 +0530

    Update settings.py

--- AllinOne/settings.py
+++ AllinOne/settings.py
@@ -38,9 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'myapp',
-    'django_filters',
-    'myapp.apps.MyappConfig'
+    'myapp',
+    # 'django_filters',
+    # 'myapp.apps.MyappConfig'
 ]
 
 MIDDLEWARE = [
