a620c9a6e731941e187697d2783440583e24a7c9
Divyesh Patel <71879941+DD-PATEL@users.noreply.github.com>
2022-01-02 05:03:22 +0530

    Update urls.py

--- AllinOne/urls.py
+++ AllinOne/urls.py
@@ -19,6 +19,10 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings 
 
+from django.conf import settings
+from django.conf.urls import url
+from django.views.static import serve
+
 admin.site.site_header = "AllinOne  Admin"
 admin.site.site_title = "AllinOne Portal"
 admin.site.index_title = "Welcome to AllinOne Portal"
@@ -26,6 +30,9 @@ admin.site.index_title = "Welcome to AllinOne Portal"
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('myapp.urls')),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
