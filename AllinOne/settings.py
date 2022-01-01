65431d5ae12c122b0c9605f22a464134dda32c9c
Divyesh Patel <71879941+DD-PATEL@users.noreply.github.com>
2022-01-02 04:13:18 +0530

    Update settings.py

--- AllinOne/settings.py
+++ AllinOne/settings.py
@@ -142,5 +142,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = '<Your Email>'
-EMAIL_HOST_PASSWORD = '<Your Secret Key>'
+EMAIL_HOST_USER = 'tronphoenix3@gmail.com'
+EMAIL_HOST_PASSWORD = 'iejwjpkdyeeorsvi'
